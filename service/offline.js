5180e06afffbe6b7e758dc73d44bd54e6a3b20d6
Deven Sitapara <shriramsoft@gmail.com>
2021-07-21 16:06:50 +0530

    mac fix - generate json file

--- service/offline.js
+++ service/offline.js
@@ -3755,7 +3755,7 @@ var generateFile = function (req, res) {
                         return callback(err);
                       }
 
-                      fs.exists(
+                      fs.access(
                         single_dir +
                           "returns" +
                           "_" +
@@ -12184,7 +12184,7 @@ var generateErrorFile = function (req, res) {
                   return callback(err);
                 }
 
-                fs.exists(
+                fs.access(
                   single_dir +
                     "returns" +
                     "_" +
